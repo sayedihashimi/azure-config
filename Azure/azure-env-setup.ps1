9c507baec3f1ab676cee1ff674abede2c197afe4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-09-15 20:54:28 -0700

    updating module

--- Azure/azure-env-setup.ps1
+++ Azure/azure-env-setup.ps1
@@ -1,11 +1,17 @@
 ﻿param(
-    [Parameter(Mandatory = $true)]
-    [string]$sourceEnvFile,
+    [string]
+    $sourceEnvFile,
 
-    [Parameter(Mandatory = $true)]
-    [string]$destEnvFile,
+    [string]
+    $destEnvFile,
 
-    [string]$azurePowershellModulePath="C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1",
+    $project = (Get-Project),
+
+    [string]
+    $envName = 'local',
+
+    [string]
+    $azurePowershellModulePath="C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1",
 
     [switch]
     $CreateNonExistingObjects
@@ -22,125 +28,20 @@ Import-Module $azurePowershellModulePath
 if((Get-Module azure-helpers)){
     Remove-Module azure-helpers
 }
-
-# TODO :Finish later
+$VerbosePreference = "Continue"
 # import the helper functions we've created
-$scriptDir = Get-ScriptDirectory
-Import-Module (Join-Path -Path $scriptDir -ChildPath 'azure-helpers.psm1')
-
+Import-Module (Join-Path -Path (Get-ScriptDirectory) -ChildPath 'azure-helpers.psm1') -PassThru
 
 # set this after importing the module
 $VerbosePreference = "Continue"
 
-$defaultSubName = 'local'
-$defaultLocalDbServerName = '(LocalDb)\v11.0'
-$defaultStorageLocation = 'West US'
-
-# TODO: GAP: We cannot get the SQL Database password, need to update the APIs to expose it or
-#            or some better runtime support for getting the password somehow
-$dbPassword = 'p@ssw0rd'
-# constants which change script behavior
-$storageUsePrimaryKey = $true
-$storageDefaultProtocol = 'https'
-$dbServerRootDomain = ".database.windows.net,1433"
-
-############ Begin script
-
-# copy the source file to the destination
-Copy-Item -LiteralPath $sourceEnvFile -Destination $destEnvFile
-
-[xml]$configXml = Get-Content $destEnvFile
-$currentAzureSubBefore = Get-AzureSubscription -Current
-
+# TODO: There must be a better way to do this
 if($CreateNonExistingObjects){
-    $currentSubscription = Get-AzureSubscription -Current
-
-    "Creating non-existing object in environment" | WriteDebugMessage
-    foreach($node in $configXml.AzureConfiguration.Environment.ChildNodes){
-        if(($node -is [System.Xml.XmlComment])){
-            continue
-        }
-
-        $envName = $configXml.AzureConfiguration.Environment.Name
-        $subName = GetSubscriptionValueForNode -xmlNode $node
-        $subNode = $configXml.AzureConfiguration.Subscriptions.Subscription | Where-Object {$_.Name -eq $subName}
-        $subId = $subNode.Id
-
-        if($subName -eq 'local'){
-            # create local resources here
-            # TODO: What needs to be done here?
-        }
-        else{
-            if($subId -ne $currentSubscription.SubscriptionId){                
-                $azSub = (Get-AzureSubscription | Where-Object {$_.SubscriptionId -eq $subNode.Id})[0]
-                # Set-AzureSubscription -SubscriptionName $azSub.SubscriptionName | Out-Null
-                Select-AzureSubscription -SubscriptionName $azSub.SubscriptionName | Out-Null
-            }
-
-            if($node.LocalName -eq 'StorageAccount'){
-                # if the storage account doesn't exit create it
-                $storageName = GetFullStorageAccountName -baseName $node.Name -subId $subId -envName $envName
-                # Get-AzureStorageAccount -StorageAccountName $node.Name | Out-Null
-                Get-AzureStorageAccount -StorageAccountName $storageName | Out-Null
-                if(!($?)){
-                    "Creating storage account [{0}]" -f $storageName | WriteDebugMessage
-                    $newStorageAcct = (New-AzureStorageAccount -StorageAccountName $storageName -Location $defaultStorageLocation)
-                    "Done creating storage account" | WriteDebugMessage
-                }
-            }
-            elseif($node.LocalName -eq 'SqlDatabase'){
-                # see if the db server exists or not
-                $dbServer = (Get-AzureSqlDatabaseServer | Where-Object {$_.ServerName -eq $node.ServerName })
-                "what happened" | WriteDebugMessage
-            }
-        }
-    }
+    UpdateAzureProjectInfo -project $project -envName $envName -sourceEnvFile $sourceEnvFile -destEnvFile $destEnvFile -CreateNonExistingObjects
+}
+else{
+    UpdateAzureProjectInfo -project $project -envName $envName -sourceEnvFile $sourceEnvFile -destEnvFile $destEnvFile 
 }
 
-# Find all StorageAccount elements under Environments and then populate the connection string
-foreach($node in $configXml.AzureConfiguration.Environment.ChildNodes){
-    if(($node -is [System.Xml.XmlComment])){
-        continue
-    }
-
-    $subName = GetSubscriptionValueForNode -xmlNode $node
-    $subNode = $configXml.AzureConfiguration.Subscriptions.Subscription | Where-Object {$_.Name -eq $subName}
-    $subId = $subNode.Id
-    
-    # see if the node has a ConnectionString element if it does skip over it
-    if(!$node.ConnectionString){                     
-        # get the connection string for the asset
-        $conString = $null
-        $envName = $configXml.AzureConfiguration.Environment.Name
-        if($node.LocalName -eq 'StorageAccount'){
-            $storageKey = GetFullStorageAccountName -baseName $node.Name -subId $subId -envName $envName
-            $conString = GetStorageConnectionString -configXml $configXml -subscriptionName $subName -storageAccountName $storageKey -envName $envName
-            # add the ConnectionString attribute to the element
-            $node.SetAttribute('ConnectionString',$conString)
-        }
-        elseif($node.LocalName -eq 'SqlDatabase'){
-            $dbServer = (Get-AzureSqlDatabaseServer | Where-Object {$_.ServerName -eq $node.ServerName })
-            $dbName = $node.Name
-
-            $dbConString = (Get-SQLAzureDatabaseConnectionString -DatabaseServerName $dbServer.ServerName -DatabaseName $node.Name -UserName $dbServer.AdministratorLogin -Password $dbPassword)
-            "DB connection string: [{0}]" -f $dbConString | WriteDebugMessage
-
-            $node.SetAttribute("ConnectionString",$dbConString)
-        }
-    }
-    else{
-        "Skipped updating ConnectionString because the element has alredy defined the attribute" | WriteDebugMessage
-    }
-}
-
-# save the new XML file to the destination
-"Saving config file with the updated contents at [{0}]" -f $destEnvFile | WriteDebugMessage
-$configXml.Save($destEnvFile)
-
-# restore the original azure subscription
-if($currentAzureSubBefore){
-    # Set-AzureSubscription -SubscriptionName $currentAzureSubBefore.SubscriptionName
-    Select-AzureSubscription -SubscriptionName $currentAzureSubBefore.SubscriptionName | Out-Null
-}
 # TODO: Remove this later
 $VerbosePreference = "SilentlyContinue"
--- Azure/azure-helpers.psm1
+++ Azure/azure-helpers.psm1
@@ -1,4 +1,15 @@
 
+$defaultSubName = 'local'
+$defaultLocalDbServerName = '(LocalDb)\v11.0'
+$defaultStorageLocation = 'West US'
+
+# TODO: GAP: We cannot get the SQL Database password, need to update the APIs to expose it or
+#            or some better runtime support for getting the password somehow
+$dbPassword = 'p@ssw0rd'
+# constants which change script behavior
+$storageUsePrimaryKey = $true
+$storageDefaultProtocol = 'https'
+$dbServerRootDomain = ".database.windows.net,1433"
 
 function WriteDebugMessage(){
     param([string] $input)
@@ -146,6 +157,108 @@ function Detect-IPAddress
     return $result
 }
 
+function UpdateFileWithEndpointInfo(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [xml]
+        $configXml,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $destEnvFile
+    )
+    # Find all StorageAccount elements under Environments and then populate the connection string
+    foreach($node in $configXml.AzureConfiguration.Environment.ChildNodes){
+        if(($node -is [System.Xml.XmlComment])){
+            continue
+        }
+
+        $subName = GetSubscriptionValueForNode -xmlNode $node
+        $subNode = $configXml.AzureConfiguration.Subscriptions.Subscription | Where-Object {$_.Name -eq $subName}
+        $subId = $subNode.Id
+    
+        # see if the node has a ConnectionString element if it does skip over it
+        if(!$node.ConnectionString){                     
+            # get the connection string for the asset
+            $conString = $null
+            $envName = $configXml.AzureConfiguration.Environment.Name
+            if($node.LocalName -eq 'StorageAccount'){
+                $storageKey = GetFullStorageAccountName -baseName $node.Name -subId $subId -envName $envName
+                $conString = GetStorageConnectionString -configXml $configXml -subscriptionName $subName -storageAccountName $storageKey -envName $envName
+                # add the ConnectionString attribute to the element
+                $node.SetAttribute('ConnectionString',$conString)
+            }
+            elseif($node.LocalName -eq 'SqlDatabase'){
+                $dbServer = (Get-AzureSqlDatabaseServer | Where-Object {$_.ServerName -eq $node.ServerName })
+                $dbName = $node.Name
+
+                $dbConString = (Get-SQLAzureDatabaseConnectionString -DatabaseServerName $dbServer.ServerName -DatabaseName $node.Name -UserName $dbServer.AdministratorLogin -Password $dbPassword)
+                "DB connection string: [{0}]" -f $dbConString | WriteDebugMessage
+
+                $node.SetAttribute("ConnectionString",$dbConString)
+            }
+        }
+        else{
+            "Skipped updating ConnectionString because the element has alredy defined the attribute" | WriteDebugMessage
+        }
+    }
+
+    # save the new XML file to the destination
+    "Saving config file with the updated contents at [{0}]" -f $destEnvFile | WriteDebugMessage
+    $configXml.Save($destEnvFile)
+}
+function CreateNonExistingObjects(){
+    param(
+        [Parameter(Mandatory=$true)]
+        [xml]
+        $configXml
+    )
+
+    $currentSubscription = Get-AzureSubscription -Current
+
+    "Creating non-existing object in environment" | WriteDebugMessage
+    foreach($node in $configXml.AzureConfiguration.Environment.ChildNodes){
+        if(($node -is [System.Xml.XmlComment])){
+            continue
+        }
+
+        $envName = $configXml.AzureConfiguration.Environment.Name
+        $subName = GetSubscriptionValueForNode -xmlNode $node
+        $subNode = $configXml.AzureConfiguration.Subscriptions.Subscription | Where-Object {$_.Name -eq $subName}
+        $subId = $subNode.Id
+
+        if($subName -eq 'local'){
+            # create local resources here
+            # TODO: What needs to be done here?
+        }
+        else{
+            if($subId -ne $currentSubscription.SubscriptionId){                
+                $azSub = (Get-AzureSubscription | Where-Object {$_.SubscriptionId -eq $subNode.Id})[0]
+                # Set-AzureSubscription -SubscriptionName $azSub.SubscriptionName | Out-Null
+                Select-AzureSubscription -SubscriptionName $azSub.SubscriptionName | Out-Null
+            }
+
+            if($node.LocalName -eq 'StorageAccount'){
+                # if the storage account doesn't exit create it
+                $storageName = GetFullStorageAccountName -baseName $node.Name -subId $subId -envName $envName
+                # Get-AzureStorageAccount -StorageAccountName $node.Name | Out-Null
+                Get-AzureStorageAccount -StorageAccountName $storageName | Out-Null
+                if(!($?)){
+                    "Creating storage account [{0}]" -f $storageName | WriteDebugMessage
+                    $newStorageAcct = (New-AzureStorageAccount -StorageAccountName $storageName -Location $defaultStorageLocation)
+                    "Done creating storage account" | WriteDebugMessage
+                }
+            }
+            elseif($node.LocalName -eq 'SqlDatabase'){
+                # see if the db server exists or not
+                $dbServer = (Get-AzureSqlDatabaseServer | Where-Object {$_.ServerName -eq $node.ServerName })
+                "what happened" | WriteDebugMessage
+            }
+        }
+    }
+}
+
+# Functions related to the VS project
 function GetProjDirectory(){
     param(
         [Parameter(Mandatory=$true)]
@@ -185,13 +298,47 @@ function GetAzureConfigFileForProject(){
     return $result
 }
 
+function GetOutputPathForProject(){
+    param(
+        $project = (Get-Project)
+    )
+
+    $outputPathForProj = ($project.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value.ToString())
+    # resolve it to a full path
+    $oldLoc = Get-Location
+    
+    Set-Location ((get-item $project.FileName).DirectoryName)
+    
+    $fullPathToOutputFolder = (Resolve-Path $outputPathForProj).ToString()
+
+    Set-Location $oldLoc
+
+    # make sure the path ends with a \ before returning to the caller
+    $fulPathToOutputFolder = $fullPathToOutputFolder.Trim()
+    if(!$fullPathToOutputFolder.EndsWith('\')){
+        $fullPathToOutputFolder += '\'
+    }
+
+    return $fullPathToOutputFolder
+}
+
+function GetProjectAzureOutputFile(){
+    param(
+        $project = (Get-Project),
+        
+        [string]
+        $envName = 'local'
+    )
+
+    return ("{0}{1}.xml" -f (GetOutputPathForProject), $envName)    
+}
+
 # This function will look in the local.xml file to see if there is an existing storage acct with the given name
 # if not then it will be added to the file and then the local.xml file
 # next time the cmd is executed to insert con strings this will be picked up
 function AddStorageAcctToProject(){
     param(
-        [Parameter(Mandatory=$true)]
-        $project,
+        $project = (Get-Project),
         
         [Parameter(Mandatory=$true)]
         [string]
@@ -223,5 +370,47 @@ function AddStorageAcctToProject(){
     $configXml.Save($configXmlPath)
 }
 
+function UpdateAzureProjectInfo(){
+    param(
+        $project = (Get-Project),
 
-Export-ModuleMember -function *
+        [string]
+        $envName = 'local',
+
+        $sourceEnvFile,
+        $destEnvFile,
+
+        [switch]
+        $CreateNonExistingObjects
+    )
+
+    if(!$sourceEnvFile){
+        $sourceEnvFile = GetAzureConfigFileForProject -projectName $project.Name -envName $envName
+    }
+    if(!$destEnvFile){
+        $destEnvFile = GetProjectAzureOutputFile -project $project -envName $envName
+    }
+
+    # TODO: Is this necessary?
+    Copy-Item -LiteralPath $sourceEnvFile -Destination $destEnvFile
+    
+    [xml]$configXml = Get-Content $destEnvFile
+    $currentAzureSubBefore = Get-AzureSubscription -Current
+
+    if($CreateNonExistingObjects){
+        "Calling CreateNonExistingObjects" | Write-Host
+        CreateNonExistingObjects -configXml $configXml
+    }
+
+    UpdateFileWithEndpointInfo -configXml $configXml -destEnvFile $destEnvFile
+
+    # restore the original azure subscription
+    if($currentAzureSubBefore){
+        # Set-AzureSubscription -SubscriptionName $currentAzureSubBefore.SubscriptionName
+        Select-AzureSubscription -SubscriptionName $currentAzureSubBefore.SubscriptionName | Out-Null
+    }
+}
+
+# Set-Alias Update-AzureProjFile UpdateFileWithEndpointInfo
+
+Export-ModuleMember -function * -Alias *
